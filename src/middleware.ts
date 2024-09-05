7ce542307a43c1541bd0e6054ffed8335e094e36
SyntaxLexx <spencermainaa@gmail.com>
2024-09-06 01:59:26 +0300

    restrict admin dashboard to admins.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,33 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { SessionClaims } from "./types";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    auth().protect();
+
+    const { sessionClaims }: { sessionClaims: SessionClaims } = auth();
+
+    // redirect admins
+    if (sessionClaims?.meta?.role === "admin") {
+      return NextResponse.redirect(new URL("/admin", req.url));
+    }
+  }
+
+  // Restrict admin routes to users with specific permissions
+  if (isAdminRoute(req)) {
+    auth().protect(() => {
+      const { sessionClaims }: { sessionClaims: SessionClaims } = auth();
+
+      if (!sessionClaims) return false;
+
+      return sessionClaims.meta?.role === "admin";
+    });
+  }
 });
 
 export const config = {
